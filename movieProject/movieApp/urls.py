bc07c51d2789a28d04cc5d67d91ad3dec6e81e09
tdude0175 <tdude0175@gmail.com>
2019-02-22 16:12:39 -0600

    added comments

--- movieProject/movieApp/urls.py
+++ movieProject/movieApp/urls.py
@@ -5,6 +5,6 @@ urlpatterns = \
     [
         path('', views.index, name='index'),
         path('index/', views.indexDirection, name='indexDirection'),
-        path('base/', views.Base, name='Base'),
+        path('base/', views.Base, name='Base'),  # i used {indexDirection} to reference the index html to avoid confusion
         path('about/', views.About, name='About')
     ]
--- movieProject/movieApp/views.py
+++ movieProject/movieApp/views.py
@@ -4,14 +4,12 @@ from django.http import HttpResponse
 
 # Create your views here.
 
-
+# simple starting point could add flavor but not worried about it
 def index(request):
     return HttpResponse("welcome")
 
-
+# these 3 views just reference the page based on name, not much new
 def indexDirection(request):
-    print("test")
-    # return HttpResponse("Test")
     return render(request, 'movieApp/index.html')
 
 
